9d8ca62a5555db773c96c548fb6bbb73b8eb7145
DOZIEZ <93401480+DOZIEZ@users.noreply.github.com>
2021-11-11 15:02:47 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// const OpenNav= document.querySelector(".icon1");
+// const FermerNav= document.querySelector(".fermer");
+// const Menu= document.querySelector(".menu");
+
+// const PositionMenu= Menu.getBoundingClientRect().left;
+
+// OpenNav.addEventListener("click", ()=>{
+//     if(PositionMenu<0){
+//         Menu.classList.add("Monter")
+//     };
+// });
+
+
+// FermerNav.addEventListener("click", ()=>{
+//     if(PositionMenu<0){
+//         Menu.classList.remove("Monter")
+//     };
+// });
